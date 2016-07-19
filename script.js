9436be98d345a3d359bf7c63ff114c35240cb441
wilsun007 <williamjinsun@gmail.com>
2016-07-19 00:56:14 -0700

    First Push
    -Sketch mockups done
    -Home Page finished

---
+++ script.js
@@ -0,0 +1,8 @@
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+$(document).ready(function() {
+    $("[href]").each(function() {
+    if (this.href == window.location.href) {
+        $(this).addClass("current");
+        }
+    });
+});
